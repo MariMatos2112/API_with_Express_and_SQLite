fb65f9aec82098e2d89018b2a38f98174803f8ea
MariMatos2112 <maristudies2112@gmail.com>
2022-01-14 16:30:06 -0300

    treatment of json and form requests

--- API_with_express_and_MYSQL/controllers/atendimentos.js
+++ API_with_express_and_MYSQL/controllers/atendimentos.js
@@ -1,5 +1,10 @@
-module.exports = app => {
-    app.get('/atendimentos', (req, resp) => resp.send('This GET route is working ;)'))
+module.exports = (app) => {
+  app.get("/atendimentos", (req, resp) =>
+    resp.send("This GET route is working ;)")
+  );
 
-    app.post('/atendimentos', (req, resp) => resp.send('This POST route is working ;)'))
-}
+  app.post("/atendimentos", (req, resp) => {
+    console.log(req.body);
+    resp.send("This POST route is working ;)");
+  });
+};
