c5f9422ccd6e615e6c2a07b3d7aaa11ab874af83
MariMatos2112 <maristudies2112@gmail.com>
2022-01-18 00:18:30 -0300

    fixing and improving deleteData.js

--- API_with_express_and_MYSQL/database/deleteData.js
+++ API_with_express_and_MYSQL/database/deleteData.js
@@ -5,11 +5,19 @@ const db = new sqlite3.Database("./database/db.sqlite", (error) => {
 });
 
 exports.deleteSongById = (response, id) => {
-  db.all(`DELETE FROM saved_songs WHERE id = ${id}`, (error, data) => {
-    if (error) {
-      response.status(404);
-      response.send(`ERROR: The column "${id}" was not found.`);
+  db.all(`SELECT * FROM saved_songs WHERE id = ${id}`, (error, data) => {
+    if (data) {
+      if (data.length === 0) {
+        response.status(404);
+        response.send(`ERROR: The column "${id}" was not found.`);
+      } else {
+        db.all(`DELETE FROM saved_songs WHERE id = ${id}`, () => {
+          response.send("SUCCESS: The song was deleted successfully!");
+        });
+      }
+    } else {
+      response.status(400);
+      response.send("ERROR: The ID should be an integer, not a string.");
     }
-    else response.send('SUCCESS: The song was deleted successfully.');
   });
 };
